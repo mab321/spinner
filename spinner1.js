1dfbb839446b85ff10168dbbb07a0fc5491fec5e
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-16 02:55:30 +0000

    Created and added spinner.js low fidelity loading spinner

---
+++ spinner1.js
@@ -0,0 +1,12 @@
+let shape = ['|','/','-','\\','|','/','-','\\','|','\n'];
+
+let duration = 100;
+for(const elm of shape){
+setTimeout(() => {
+  process.stdout.write(`\r ${elm}`);
+}, duration);
+ duration += 200;
+}
+
+
+

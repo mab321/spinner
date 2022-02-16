5e5e32de63239d20d53d5b186e5aa2e3e9b821f4
Monther Balghonaim <monther.balghonaim@gmail.com>
2022-02-16 03:01:59 +0000

    Created and added spinner.js low-fidelity spinner

--- spinner1.js
+++ spinner1.js
@@ -1,12 +1,44 @@
-let shape = ['|','/','-','\\','|','/','-','\\','|','\n'];
-
-let duration = 100;
-for(const elm of shape){
 setTimeout(() => {
-  process.stdout.write(`\r ${elm}`);
-}, duration);
- duration += 200;
-}
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
+
+// ... fill in the rest yourself ...
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1500);
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 1700);
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\n'); 
+}, 1900);
 
 
 
